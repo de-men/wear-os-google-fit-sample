a8cfc7b5f0119affd82c9341e66ba8b1ef626028
Thai Huynh <thai.huynh.xyz@gmail.com>
2019-06-25 13:28:00 +0700

    Access to Google Fit

--- app/src/main/java/org/demen/wearosgooglefitsample/MainActivity.kt
+++ app/src/main/java/org/demen/wearosgooglefitsample/MainActivity.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.auth.api.signin.GoogleSignInResult
@@ -16,6 +17,13 @@ import com.google.android.gms.common.SignInButton
 import com.google.android.gms.common.api.GoogleApiClient
 import com.google.android.gms.common.api.ResultCallback
 import com.google.android.gms.common.api.Status
+import com.google.android.gms.fitness.Fitness
+import com.google.android.gms.fitness.FitnessActivities
+import com.google.android.gms.fitness.data.Session
+import com.google.android.gms.fitness.request.SessionInsertRequest
+import com.google.android.gms.fitness.request.SessionReadRequest
+import java.util.*
+import java.util.concurrent.TimeUnit
 
 class MainActivity : WearableActivity(), GoogleApiClient.ConnectionCallbacks,
     GoogleApiClient.OnConnectionFailedListener, ResultCallback<Status> {
@@ -24,6 +32,8 @@ class MainActivity : WearableActivity(), GoogleApiClient.ConnectionCallbacks,
     private var mGoogleSignInAccount: GoogleSignInAccount? = null
     private var mSignInButton: SignInButton? = null
     private var mSignOutButton: Button? = null
+    private var mSendFitButton: Button? = null
+    private var mReadFitButton: Button? = null
     private var mUserIdToken: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,9 +47,35 @@ class MainActivity : WearableActivity(), GoogleApiClient.ConnectionCallbacks,
 
         mSignInButton = findViewById(R.id.sign_in_button)
         mSignOutButton = findViewById(R.id.sign_out_button)
+        mSendFitButton = findViewById(R.id.send_fit)
+        mReadFitButton = findViewById(R.id.read_fit)
 
         mSignInButton?.setOnClickListener { signIn() }
         mSignOutButton?.setOnClickListener { signOut() }
+        mSendFitButton?.setOnClickListener {
+            if (!GoogleSignIn.hasPermissions(mGoogleSignInAccount, Fitness.SCOPE_ACTIVITY_READ_WRITE)) {
+                GoogleSignIn.requestPermissions(
+                    this, // your activity
+                    GOOGLE_FIT_PERMISSIONS_REQUEST_CODE,
+                    mGoogleSignInAccount,
+                    Fitness.SCOPE_ACTIVITY_READ_WRITE
+                )
+            } else {
+                sendFit(mGoogleSignInAccount!!)
+            }
+        }
+        mReadFitButton?.setOnClickListener {
+            if (!GoogleSignIn.hasPermissions(mGoogleSignInAccount, Fitness.SCOPE_ACTIVITY_READ_WRITE)) {
+                GoogleSignIn.requestPermissions(
+                    this, // your activity
+                    GOOGLE_FIT_PERMISSIONS_REQUEST_CODE,
+                    mGoogleSignInAccount,
+                    Fitness.SCOPE_ACTIVITY_READ_WRITE
+                )
+            } else {
+                readFit()
+            }
+        }
     }
 
     override fun onResume() {
@@ -60,6 +96,8 @@ class MainActivity : WearableActivity(), GoogleApiClient.ConnectionCallbacks,
         if (requestCode == RC_SIGN_IN) {
             val result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
             handleSignInResult(result)
+        } else if (requestCode == GOOGLE_FIT_PERMISSIONS_REQUEST_CODE) {
+            Toast.makeText(this, "Request Google Fit Permission successful!", Toast.LENGTH_SHORT).show()
         }
     }
 
@@ -67,10 +105,9 @@ class MainActivity : WearableActivity(), GoogleApiClient.ConnectionCallbacks,
      * Configures the GoogleApiClient used for sign in. Requests scopes profile and email.
      */
     private fun setupGoogleApiClient() {
-        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestProfile()
+        val gso = GoogleSignInOptions.Builder()
             .requestEmail()
-            .requestIdToken(getString(R.string.server_client_id))
+            .requestScopes(Fitness.SCOPE_ACTIVITY_READ_WRITE)
             .build()
         mGoogleApiClient = GoogleApiClient.Builder(this)
             .addOnConnectionFailedListener(this)
@@ -95,7 +132,8 @@ class MainActivity : WearableActivity(), GoogleApiClient.ConnectionCallbacks,
 
                 mSignInButton?.visibility = View.GONE
                 mSignOutButton?.visibility = View.VISIBLE
-
+                mSendFitButton?.visibility = View.VISIBLE
+                mReadFitButton?.visibility = View.VISIBLE
             }
         } else {
             Log.d(TAG, "Google Sign-In failure: " + result.status)
@@ -135,8 +173,97 @@ class MainActivity : WearableActivity(), GoogleApiClient.ConnectionCallbacks,
             Log.e(TAG, "Google API client not initialized or not connected")
             return
         }
-        mGoogleApiClient!!.connect()
-        Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(this)
+
+        val signInOptions = GoogleSignInOptions.Builder()
+            .requestScopes(Fitness.SCOPE_ACTIVITY_READ_WRITE)
+            .build()
+        GoogleSignIn.getClient(this, signInOptions).revokeAccess()
+            .addOnSuccessListener {
+                Log.d(TAG, "disableFit onSuccess")
+                mSendFitButton?.visibility = View.GONE
+                mReadFitButton?.visibility = View.GONE
+                Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(this)
+            }
+            .addOnFailureListener { e ->
+                Log.e(TAG, "disableFit onFailure", e)
+                mSendFitButton?.visibility = View.GONE
+                mReadFitButton?.visibility = View.GONE
+                Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(this)
+            }
+    }
+
+    private fun sendFit(account: GoogleSignInAccount) {
+        Log.d(TAG, "sendFit")
+
+        val cal = Calendar.getInstance()
+        cal.time = Date()
+        val endTime = cal.timeInMillis
+        cal.add(Calendar.DATE, -1)
+        cal.set(Calendar.HOUR_OF_DAY, 0)
+        cal.set(Calendar.MINUTE, 0)
+        cal.set(Calendar.SECOND, 0)
+        cal.set(Calendar.MILLISECOND, 0)
+        val startTime = cal.timeInMillis
+
+        // Create the sleep session
+        val session = Session.Builder()
+            .setIdentifier("id_$startTime")
+            .setName("session_name")
+            .setDescription("session for test")
+            .setStartTime(startTime, TimeUnit.MILLISECONDS)
+            .setEndTime(endTime, TimeUnit.MILLISECONDS)
+            .setActivity(FitnessActivities.SLEEP)
+            .build()
+
+        // Build the request to insert the session.
+        val request = SessionInsertRequest.Builder()
+            .setSession(session)
+            .build()
+
+        // Insert the session into Fit platform
+        Log.i(TAG, "Inserting the session in the Sessions API")
+        Fitness.getSessionsClient(this, account)
+            .insertSession(request)
+            .addOnSuccessListener {
+                Log.i(TAG, "Session insert was successful!")
+            }
+            .addOnFailureListener { e ->
+                Log.i(TAG, "There was a problem inserting the session: ${e.localizedMessage}")
+            }
+    }
+
+    private fun readFit() {
+        Log.d(TAG, "readFit")
+
+        val cal = Calendar.getInstance()
+        cal.time = Date()
+        val endTime = cal.timeInMillis
+        cal.add(Calendar.DATE, -1)
+        cal.set(Calendar.HOUR_OF_DAY, 0)
+        cal.set(Calendar.MINUTE, 0)
+        cal.set(Calendar.SECOND, 0)
+        cal.set(Calendar.MILLISECOND, 0)
+        val startTime = cal.timeInMillis
+
+
+        // Build a session read request
+        val readRequest = SessionReadRequest.Builder()
+            .setTimeInterval(startTime, endTime, TimeUnit.MILLISECONDS)
+            .readSessionsFromAllApps()
+            .build()
+
+        Fitness.getSessionsClient(this, mGoogleSignInAccount!!)
+            .readSession(readRequest)
+            .addOnSuccessListener { sessionReadResponse ->
+                Log.d(TAG, "readSession onSuccess: ${sessionReadResponse.sessions}")
+            }
+            .addOnFailureListener { e ->
+                Log.e(TAG, "readSession onFailure", e)
+            }
+            .addOnCompleteListener {
+                Log.d(TAG, "readSession onComplete")
+            }
+
     }
 
     override fun onConnectionFailed(p0: ConnectionResult) {
@@ -167,5 +294,6 @@ class MainActivity : WearableActivity(), GoogleApiClient.ConnectionCallbacks,
     companion object {
         const val TAG = "MainActivity"
         const val RC_SIGN_IN = 8001
+        const val GOOGLE_FIT_PERMISSIONS_REQUEST_CODE = 8002
     }
 }
